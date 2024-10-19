c471f0619f2924e896f434e71d9f541367f97b14
Farhat Chayma <chayma.farhat@sousse.r-iset.tn>
2024-10-19 23:52:12 +0100

    environment files commit

--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +12,6 @@ export class GithubService {
 
   private getHeaders() {
     return new HttpHeaders({
-      'Authorization': `token ${environment.githubToken}`,
       'Accept': 'application/vnd.github.v3+json'
     });
   }
--- src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-import * as dotenv from 'dotenv';
-dotenv.config();
-
-export const environment = {
-    production: false,
-    githubToken: process.env['GITHUB_TOKEN']
-};
