536798e910f20e24963d88e2c5128d9b1e284021
Farhat Chayma <chayma.farhat@sousse.r-iset.tn>
2024-10-19 21:41:48 +0100

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { forkJoin } from 'rxjs';
+import { GithubService } from './services/github.service';
+import { NotificationService } from './services/notification.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'githubHealthChecker';
+  owner: string = 'academico-sis';
+  repo: string = 'academico';
+  repository: any;
+  pullRequests: any[] = [];
+  commitActivity: any[] = [];
+  daysSinceLastCommit: number = 0;
+  contributorsCount: number = 0;
+  loading: boolean = false;
+
+  constructor(private githubService: GithubService, private notificationService: NotificationService) { }
+
+  checkRepoHealth() {
+    this.loading = true;
+    forkJoin({
+      repositoryData: this.githubService.getRepositoryData(this.owner, this.repo),
+      commitActivity: this.githubService.getCommitActivity(this.owner, this.repo),
+      contributors: this.githubService.getContributors(this.owner, this.repo),
+      pullRequests: this.githubService.getPullRequests(this.owner, this.repo)
+    }).subscribe({
+      next: ({ repositoryData, commitActivity, contributors, pullRequests }) => {
+        this.repository = repositoryData;
+        const lastPush = new Date(repositoryData.pushed_at);
+        const currentDate = new Date();
+        this.daysSinceLastCommit = Math.floor((currentDate.getTime() - lastPush.getTime()) / (1000 * 60 * 60 * 24));
+        this.commitActivity = commitActivity;
+        this.contributorsCount = contributors.length;
+        this.pullRequests = pullRequests;
+      },
+      error: (error) => {
+        this.notificationService.showError('Error fetching repository data. Please try again later.');
+      },
+      complete: () => {
+        this.loading = false;
+      }
+    });
+  }
 }
---
+++ src/app/services/github.service.ts
@@ -0,0 +1,44 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GithubService {
+  private apiUrl = 'https://api.github.com/repos/';
+
+  constructor(private http: HttpClient) { }
+
+  private getHeaders() {
+    const token = this.getToken();
+    return new HttpHeaders({
+      'Authorization': `token ${token}`,
+      'Accept': 'application/vnd.github.v3+json'
+    });
+  }
+
+  private getToken(): string {
+    return 'environment.githubToken';
+  }
+
+  getRepositoryData(owner: string, repo: string): Observable<any> {
+    const url = `${this.apiUrl}${owner}/${repo}`;
+    return this.http.get<any>(url, { headers: this.getHeaders() });
+  }
+
+  getCommitActivity(owner: string, repo: string): Observable<any> {
+    const url = `${this.apiUrl}${owner}/${repo}/commits`;
+    return this.http.get<any>(url, { headers: this.getHeaders() });
+  }
+
+  getPullRequests(owner: string, repo: string): Observable<any> {
+    const url = `${this.apiUrl}${owner}/${repo}/pulls`;
+    return this.http.get<any>(url, { headers: this.getHeaders() });
+  }
+
+  getContributors(owner: string, repo: string): Observable<any> {
+    const url = `${this.apiUrl}${owner}/${repo}/contributors`;
+    return this.http.get<any>(url, { headers: this.getHeaders() });
+  }
+}
